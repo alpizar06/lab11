27c193be5c53b4f1830ff7abc6d8bd0497b5edc7
Daniel Alpizar <alpizar0610@gmail.com>
2021-10-09 23:57:41 -0600

    Add files via upload

---
+++ userdefault/userdefaultApp.swift
@@ -0,0 +1,17 @@
+//
+//  userdefaultApp.swift
+//  userdefault
+//
+//  Created by Daniel Alpizar on 9/10/21.
+//
+
+import SwiftUI
+
+@main
+struct userdefaultApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
